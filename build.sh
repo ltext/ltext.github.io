25cb48f8a3e1b040a386ea4e3b032101670a27d5
Athan Clark <athan.clark@gmail.com>
2015-05-31 08:57:21 -0600

    found a bug!

--- build.sh
+++ build.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 ltext "src/wrapper.html (src/content.html src/preview.html src/usage.html \
-       src/download.html src/advanced.html)" -o index.html
+       src/download.html (src/advanced.html src/typechecking.html))" -o index.html
+
+#
+# (#src/wrapper.html) <>
+# (
+#  (
+#   (
+#    (
+#     (
+#      (
+#       (
+#        (
+#         (
+#          (#src/content.html) <>
+#          (
+#           (#src/advanced.html) <>
+#            (
+#             (#src/typechecking.html) <> (#src/advanced.html)
+#            )
+#          )
+#         ) <> (#src/content.html)
+#        ) <> (#src/download.html)
+#       ) <> (#src/content.html)
+#      ) <> (#src/usage.html)
+#     ) <> (#src/content.html)
+#    ) <> (#src/preview.html)
+#   ) <> (#src/content.html)
+#  ) <> (#src/wrapper.html)
+# )
