fec9031ba5e119b57010eb9468fd135d3366c71c
Athan Clark <athan.clark@gmail.com>
2015-06-01 11:36:34 -0600

    better documentation

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ltext "src/wrapper.html (src/content.html src/preview.html src/usage.html \
-       src/download.html (src/advanced.html src/typechecking.html src/partial.html))" -o index.html
+       src/download.html (src/advanced.html src/typechecking.html src/partial.html src/debugging.html))" -o index.html
