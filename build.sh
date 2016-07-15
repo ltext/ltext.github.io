2e4f57593e94061ef1e337cf95d787cd23ed9a83
Athan Clark <athan.clark@gmail.com>
2016-07-15 12:20:37 -0600

    update to latest version

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 ltext "src/wrapper.html (src/content.html src/preview.html src/usage.html \
-       src/download.html (src/advanced.html src/typechecking.html src/partial.html src/debugging.html))" -o index.html
+       src/download.html (src/advanced.html src/typechecking.html src/partial.html \
+       src/debugging.html))" --raw "src/preview.html" --raw "src/usage.html" \
+      --raw "src/download.html"  --raw "src/typechecking.html" --raw "src/partial" \
+      --raw "src/debugging" --raw "src/debugging.html" > index.html
